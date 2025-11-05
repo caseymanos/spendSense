b1d897de5a1228311c6fc7aef6d451bbe91608d1
Casey Manos <manoscasey@gmail.com>
2025-11-05 12:35:07 -0600

    web: add theme variants (glass/minimal/vibrant) and selector; update root README to Next.js web

--- web/providers/ThemeProvider.tsx
+++ web/providers/ThemeProvider.tsx
@@ -1,13 +1,16 @@
-'use client';
+'use client'
 
-import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+const THEMES = ['light', 'dark', 'glass', 'minimal', 'vibrant'] as const
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem={false} themes={THEMES as unknown as string[]}>
       {children}
     </NextThemesProvider>
-  );
+  )
 }
 
+export const AVAILABLE_THEMES = THEMES
